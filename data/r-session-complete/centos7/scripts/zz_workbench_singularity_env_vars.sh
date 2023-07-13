a5835ecea793b92f49cd71f017700e12396888ea
Michael Mayer <michael.mayer@posit.co>
2023-07-13 16:18:07 +0000

    minor fixes and adding SLURM_CONF definition in profile script

--- data/r-session-complete/centos7/scripts/zz_workbench_singularity_env_vars.sh
+++ data/r-session-complete/centos7/scripts/zz_workbench_singularity_env_vars.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+export SLURM_CONF=/opt/slurm/etc/slurm.conf
+
 SESSION_ID=`echo $RS_SESSION_URL | awk -F/ '{print $3}'`
 
-if (which squeue >/dev/null) && [ ! -z $SESSION_ID ]; then 
+if (which squeue >/dev/null) && [ ! -z $SESSION_ID ]; then
   SLURM_ID=`squeue -o '%.7i %55j' | grep $SESSION_ID | awk '{print $1}'`
   env_file="/tmp/.slurm-$USER-$SLURM_ID.env"
-  if [ ! -z $SLURM_ID && -f $env_file ]; then 
+  if [ -n "$SLURM_ID" ]  && [ -f $env_file ]; then
     source $env_file
   fi
 fi
