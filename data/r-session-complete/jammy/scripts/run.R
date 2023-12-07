f16ad3fcde03346b37a23cd7496bf6a740fee50a
Michael Mayer <michael.mayer@posit.co>
2023-12-07 12:46:15 +0100

    increase timeout for packagemanager

--- data/r-session-complete/jammy/scripts/run.R
+++ data/r-session-complete/jammy/scripts/run.R
@@ -33,6 +33,9 @@ pmurl <- "https://packagemanager.posit.co"
 # place to create rstudio integration for package repos
 rsconfigdir <- "/opt/rstudio/etc/rstudio" 
 
+# increase timeout for packagemanager
+options(timeout = max(300, getOption("timeout")))
+
 binaryflag<-""
 
 if(file.exists("/etc/debian_version")) {
