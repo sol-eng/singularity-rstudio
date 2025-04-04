971de32e2e0194b49fc77d50efcadd9e3f81a2e7
Michael Mayer <michael.mayer@posit.co>
2025-04-04 14:43:02 +0200

    fix session component

--- data/r-session-complete/scripts/install-session-components.sh
+++ data/r-session-complete/scripts/install-session-components.sh
@@ -4,6 +4,6 @@ DISTRO=$1
 PWB_VERSION=$2
 
 mkdir -p /usr/lib/rstudio-server
-curl -O https://s3.amazonaws.com/rstudio-ide-build/session/focal/amd64/rsp-session-${DISTRO}-${PWB_VERSION}-amd64.tar.gz
+curl -O https://s3.amazonaws.com/rstudio-ide-build/session/${DISTRO}/amd64/rsp-session-${DISTRO}-${PWB_VERSION}-amd64.tar.gz
 tar xfz rsp-session-${DISTRO}-${PWB_VERSION}-amd64.tar.gz -C /usr/lib/rstudio-server --strip=1
 rm -f rsp-session-${DISTRO}-${PWB_VERSION}-amd64.tar.gz
