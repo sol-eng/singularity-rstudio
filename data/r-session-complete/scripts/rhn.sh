193615d95dc9cd29d0ce294e91d02997fc88a1d9
Michael Mayer <michael.mayer@posit.co>
2026-05-21 16:25:29 +0200

    fix rhel8/9 repos to make R 4.6 find flexiblas-devel

--- data/r-session-complete/scripts/rhn.sh
+++ data/r-session-complete/scripts/rhn.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 subscription-manager register --username XXX --password XXX
+
+
+dnf config-manager --set-disabled "*ubi*"
+
+subscription-manager repos \
+    --enable=rhel-$1-for-x86_64-baseos-rpms \
+    --enable=rhel-$1-for-x86_64-appstream-rpms
